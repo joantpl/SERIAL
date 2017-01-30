23ea8fdfe613b9893a28672ebe940666d244e245
joantpl <jotopl13@gmail.com>
2017-01-30 21:17:13 +0100

    serial part 1
    Utilització de print i println,delay,col·locació de signes de
    programació etc.

--- Serial_2/Serial_2.ino
+++ Serial_2/Serial_2.ino
@@ -11,17 +11,24 @@
 
 
 //****** Setup  **********************************************************
-void setup()              
+ void setup()                // run once, when the sketch starts
 {
-  Serial.begin(9600);    
+  Serial.begin(9600);       // set up Serial library at 9600 bps
+  
 }
 
 //**** Loop  *************************************************************
-void loop()                     
+void loop()                 // run over and over again
 {
-  Serial.print("Hello world!");  // prints Hello world!
-  delay(1000);
+  Serial.println( "Hello world!" );  // Serial println significa escriure en aquest cas hello world en una nova linia.
+  Serial.println( "new line" ); //perque es veigui mès clar afegeixo un Serial.println que digui new line.
+  delay(1000);                  // Delay significa el temps que tardara en repetir-se la acció expressat en mil·lisegons,en aquest cas 1 seg.
 }
+
+
+
+
+
 
 
 
--- Serial_3/Serial_3.ino
+++ Serial_3/Serial_3.ino
@@ -11,17 +11,27 @@
 
 
 //****** Setup  **********************************************************
-void setup()              
+ void setup()                // run once, when the sketch starts
 {
-  Serial.begin(9600);    
+  Serial.begin(9600);       // set up Serial library at 9600 bps
+  
+  Serial.println( "Escull el numero de l'operacio que vol realitzar" );  // Per fer aquest programa fiquem serial begin 9800 per comunicar amb l'ordinador, i a cada frase que volem fer sortir la posem en un print ln per que ho faci en una linia nova i entre cometes i parentesis
+Serial.println( "1.Comprovar numero de tarjeta de credit? " ); 
+ Serial.println( "2.Comprovar numero de compte bancari?");
+Serial.println( "3.Buscar un digit perdut de la tarjeta de credit?");
+                   
 }
 
 //**** Loop  *************************************************************
-void loop()                     
-{
-  Serial.print("Hello world!");  // prints Hello world!
-  delay(1000);
+void loop()   {
+               
 }
+
+
+
+
+
+
 
 
 
