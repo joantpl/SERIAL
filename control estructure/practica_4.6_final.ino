51f0988e6bd73766e9684678680cc53b8877c196
joantpl <jotopl13@gmail.com>
2017-03-13 12:51:17 +0100

    Add files via upload

---
+++ control estructure/practica_4.6_final.ino
@@ -0,0 +1,34 @@
+/*************************************************************************
+**                                                                      **
+**                                  Serial                              **
+**       Joan torrescasana                              5/03/2017       **
+*************************************************************************/
+
+//****** Includes  *******************************************************
+
+
+//****** Variables *******************************************************
+int comptar = 11;
+
+//****** Setup ***********************************************************
+
+void setup()                    // run once, when the sketch starts
+{
+  Serial.begin(9600);          //  Conecta amb el terminal 
+  Serial.print("Ara comptare de 0 a ");   // escriu literalment el text que hi ha entre cometes
+  Serial.println(comptar);               //escriu el numero que està definitla variable 
+  for (int i=0; i <= comptar; i++)      //diu que mentres la variable i sigui inferior a la variable comptar(11) i incrementara, 
+  {                                    
+    Serial.print(i);    //cada cop que incrementa escriu el numero
+    if ( i<11){
+    Serial.print("-");//cada cop que incrementa escriu - , ja que el for indica que s'inicia un cicle que no para fins que es compleix la condició en aquest cas i<11
+    }
+    }
+  } 
+  
+
+void loop()     // we need this to be here even though its empty
+{
+}
+
+
---
+++ control estructure/practica_4.8.ino
@@ -0,0 +1,30 @@
+/*************************************************************************
+**                                                                      **
+**                                  Serial                              **
+**       Joan torrescasana                              5/03/2017       **
+*************************************************************************/
+
+//****** Includes  *******************************************************
+
+
+//****** Variables *******************************************************
+int comptar = 21;
+int i = 0;
+//****** Setup ***********************************************************
+
+void setup()               // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+  Serial.print("Ara comptare de 0 a ");
+  Serial.println(comptar);  
+   while(i <= comptar)      // EL WHILE VA ACOOMPANYAT D'UNA CONDICIÓ, MENTRES AQUESTA ES COMPLEIXI, TOT EL QUE HI HAIGUI ENTRE LES CLAUS EL PROGRAMA HO FARA.
+  {
+    Serial.print(i);       //A diferencia del for que el que fa es cear un cicle dins de les claus fins que es compleixi la condició
+    Serial.print("-");
+    i++;
+  } 
+}
+void loop()   // we need this to be here even though its empty
+{
+}
+
